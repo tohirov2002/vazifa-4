ffaa6f6a0c21e91f30fdc8476888222e5cb992cd
tohirov2002 <tohirovahmad1519@gmail.com>
2024-03-31 23:22:45 +0500

    Commit message

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import register_view,login_view,logout_view
+from .views import register_view,login_view,logout_view,email_view
 
 urlpatterns = [
     path('register/', register_view, name='register_page'),
     path('login/', login_view, name='login_page'),
+    path('email/', email_view, name='email_page'),
     path('logout/', logout_view, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+from config.settings import EMAIL_HOST_USER
 
 
 def register_view(request):
@@ -31,3 +35,20 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def email_view(request):
+    if request.method == 'POST':
+        try:
+            send_mail(
+                subject=request.POST['subject'],
+                message=request.POST['message'],
+                from_email=EMAIL_HOST_USER,
+                # from_email='info@gmail.com',
+                recipient_list=[request.POST['email'], 'tohirovahmad1519@gmail.com'],
+            )
+            return HttpResponse('<h1>Success</h1>')
+        except Exception as e:
+            return HttpResponse(f'<h1>Something went wrong</h1><p>{e}</p>', status=500)
+    return render(request, 'users/email.html')
+
